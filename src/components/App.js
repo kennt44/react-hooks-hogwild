575a63c32c0e62b8b8054d8d3406499fb47b431c
kenneth <kennjorothuo@gmail.com>
2024-11-03 20:32:50 +0300

    initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,41 @@
-import React from "react";
-import Nav from "./Nav";
+import React, { useState } from 'react';
+import { Container, Card } from 'semantic-ui-react';
+import porkersData from './data/porkers_data'; // Make sure this is your correct data import
+import HogTile from './components/HogTile';
+import HogDetail from './components/HogDetail';
+import FilterSort from './components/FilterSort';
+import HogForm from './components/HogForm';
 
-import hogs from "../porkers_data";
+const App = () => {
+  const [hogs, setHogs] = useState(porkersData);
+  const [selectedHog, setSelectedHog] = useState(null);
+  const [showGreased, setShowGreased] = useState(false);
+  const [sortOrder, setSortOrder] = useState('');
 
-function App() {
-	return (
-		<div className="App">
-			<Nav />
-		</div>
-	);
-}
+  const filteredHogs = showGreased ? hogs.filter(hog => hog.greased) : hogs;
+  const sortedHogs = [...filteredHogs].sort((a, b) => {
+    if (sortOrder === 'name') return a.name.localeCompare(b.name);
+    if (sortOrder === 'weight') return a.weight - b.weight;
+    return 0;
+  });
+
+  const toggleGreased = () => setShowGreased(!showGreased);
+  const handleSort = (order) => setSortOrder(order);
+  const addHog = (newHog) => setHogs([...hogs, newHog]);
+
+  return (
+    <Container>
+      <h1>HogWild</h1>
+      <FilterSort onFilter={toggleGreased} onSort={handleSort} onToggleGreased={toggleGreased} />
+      <Card.Group>
+        {sortedHogs.map(hog => (
+          <HogTile key={hog.name} hog={hog} onClick={setSelectedHog} />
+        ))}
+      </Card.Group>
+      {selectedHog && <HogDetail hog={selectedHog} />}
+      <HogForm addHog={addHog} />
+    </Container>
+  );
+};
 
 export default App;
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,18 +1,43 @@
-import React from "react";
-import piggy from "../assets/porco.png";
+// src/App.js
+import React, { useState } from 'react';
+import hogs from './porkers_data';
+import Nav from './components/Nav';
+import HogTile from './components/HogTile';
+import HogForm from './components/HogForm';
 
-const Nav = () => {
-	return (
-		<div className="navWrapper">
-			<span className="headerText">HogWild</span>
-			<div className="TwirlyPig">
-				<img src={piggy} className="App-logo" alt="piggy" />
-			</div>
-			<span className="normalText">
-				A React App for County Fair Hog Fans
-			</span>
-		</div>
-	);
+const App = () => {
+    const [hogsData, setHogsData] = useState(hogs);
+    const [showGreased, setShowGreased] = useState(false);
+    const [sortBy, setSortBy] = useState('name');
+    const [hiddenHogs, setHiddenHogs] = useState([]);
+
+    const toggleGreased = () => setShowGreased(!showGreased);
+    const handleSortChange = (e) => setSortBy(e.target.value);
+    const handleHideHog = (name) => setHiddenHogs([...hiddenHogs, name]);
+
+    const filteredHogs = showGreased ? hogsData.filter(hog => hog.greased) : hogsData;
+    const sortedHogs = filteredHogs.sort((a, b) => {
+        if (sortBy === 'name') return a.name.localeCompare(b.name);
+        return a.weight - b.weight;
+    });
+
+    const addHog = (newHog) => {
+        setHogsData([...hogsData, newHog]);
+    };
+
+    return (
+        <div>
+            <Nav toggleGreased={toggleGreased} handleSortChange={handleSortChange} />
+            <HogForm addHog={addHog} />
+            <div className="ui grid container">
+                {sortedHogs.map(hog => (
+                    !hiddenHogs.includes(hog.name) && (
+                        <HogTile key={hog.name} hog={hog} handleHideHog={handleHideHog} />
+                    )
+                ))}
+            </div>
+        </div>
+    );
 };
 
-export default Nav;
+export default App;
